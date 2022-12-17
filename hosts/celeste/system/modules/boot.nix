1aad340bcc09e13bcf0f8b487f02a665181bbe09
Interfiber <webmaster@interfiber.dev>
2022-12-17 15:37:42 -0500

    bootloader updates

--- hosts/celeste/system/modules/boot.nix
+++ hosts/celeste/system/modules/boot.nix
@@ -1,18 +1,19 @@
 {pkgs, ...}:
 with builtins; {
   # Bootloader config
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.theme = ./grub_theme;
+  boot.loader.grub.enable = true; # Enable grub
+  boot.loader.grub.efiSupport = true; # EFI support in grub
+  boot.loader.grub.device = "nodev"; # No device for grub
+  boot.loader.grub.useOSProber = true; # Enable OSprober
+  boot.loader.grub.theme = ./grub_theme; # Set grub theme
+  boot.loader.grub.gfxmodeEfi = "3840x2160"; # Fix grub resolution
 
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.canTouchEfiVariables = true; # Allow the bootloader to touch efi vars
+  boot.loader.efi.efiSysMountPoint = "/boot/efi"; # EFI system mount point is /boot/efi
 
   # Kernel config
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.initrd.kernelModules = ["amdgpu"]; # Not required, nice to have though
+  boot.kernelPackages = pkgs.linuxPackages_latest; # Use latest kernel version, not the old NixOS one
+  boot.initrd.kernelModules = ["amdgpu"]; # Not required, nice to have though, starts GPU drivers before systemd starts
 
   # etc
   boot.cleanTmpDir = true; # Dont keep crap laying around, saves disk space, and fixes problems with IPC sockets sometimes
