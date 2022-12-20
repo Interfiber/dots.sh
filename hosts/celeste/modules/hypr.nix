cd4678fc08c85d573410705fce0d0d7b7d00d38f
Interfiber <webmaster@interfiber.dev>
2022-12-19 19:16:59 -0500

    update hypr

--- hosts/celeste/modules/hypr.nix
+++ hosts/celeste/modules/hypr.nix
@@ -1,10 +1,14 @@
-{pkgs, ...}:
-with builtins; {
-  home.packages = with pkgs; [
-    lxappearance
-    pkgs.callPackage
-    ../../../pkgs/swww.nix
-    {}
+{
+  pkgs,
+  lib,
+  ...
+}:
+with lib; let
+  swwwPkg = pkgs.callPackage ../../../pkgs/swww.nix {};
+in {
+  home.packages = [
+    pkgs.lxappearance
+    swwwPkg
   ];
 
   wayland.windowManager.hyprland.extraConfig = builtins.readFile ../configs/hypr/hyprland.conf;
